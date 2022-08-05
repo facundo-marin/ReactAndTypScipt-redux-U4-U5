ef29de87e93e429772007896a8272eca6e2eefbb
Facundo Marin <facundo.marin@moxit.net>
2022-08-05 12:05:12 -0300

    4.17. Reminder on Event Types

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 const RepositoriesList = () => {
+  const [term, setTerm] = useState('');
+  const onSubmit = (event: React.FormEventHandler<HTMLFormElement> | any) => {
+    event.preventDefault();
+  };
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
