f2ba06dd82d5b0581338728901a8472320f62b57
Facundo Marin <facundo.marin@moxit.net>
2022-08-05 10:09:02 -0300

    4.12. Adding Request Logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,11 +2,27 @@ import axios from 'axios';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
-const searchRepositores = (term: string) => {
+export const searchRepositores = (term: string) => {
   return async (dispatch: any) => {
     dispatch({ type: ActionType.SEARCH_RESPOSITORIES });
 
     try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (error) {
       if (error instanceof Error) {
         dispatch({
