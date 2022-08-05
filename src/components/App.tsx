762c1c230ae5d843d1100a5534a7b90449b76fad
Facundo Marin <facundo.marin@moxit.net>
2022-08-05 12:03:52 -0300

    4.16. Opps.. Initial State

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { store } from '../state/store';
+import { store } from '../state';
 import RepositoriesList from './RepositoriesList';
 
 const App = () => {
