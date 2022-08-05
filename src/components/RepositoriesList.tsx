c88e834167d624caefc421479a2df17dabd7e6f0
Facundo Marin <facundo.marin@moxit.net>
2022-08-05 14:14:52 -0300

    4.21. Awkward Typings Around React-Redux

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import { useActions } from '../hooks/useActions';
+import { useSelector } from 'react-redux';
 
 const RepositoriesList = () => {
   const [term, setTerm] = useState('');
   const { searchRepositores } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: any) => state.repositories
+  );
+  console.log(data);
   const onSubmit = (event: React.FormEventHandler<HTMLFormElement> | any) => {
     event.preventDefault();
 
